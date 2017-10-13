6ed31cf4383e1719af369dfa90afabdb75b7727e
David Grönlund <david.gronlund@sigma.se>
2017-10-13 08:12:23 +0200

    changes

---
+++ src/productList.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+function ProductList (props) {
+    return (
+        <div>ProductList</div>
+    )
+}
+
+ProductList.propTypes = {
+    products: React.PropTypes.array.isRequired
+}
+
+export default ProductList;
